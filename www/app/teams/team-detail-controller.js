c8eed60d412392444cb5d821fa6a2d82b6b63909
jmetzler <moneymetz17@gmail.com>
2016-01-03 23:29:19 -0600

    first commit

---
+++ www/app/teams/team-detail-controller.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict';
+    
+    angular.module('eliteApp').controller('teamDetailController', ['$stateParams',teamDetailController]);
+    
+    function teamDetailController($stateParams){
+        var vm = this;
+        
+        console.log("$stateParams", $stateParams);        
+    };   
+    
+})();
